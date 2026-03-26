51bcceed6b07a38e37c7414c0dce419924e0767a
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2026-03-26 13:29:22 +0100

    pin setuptools instead

--- webviz_4d/__init__.py
+++ webviz_4d/__init__.py
@@ -1,25 +1,7 @@
-import sys
+from pkg_resources import get_distribution, DistributionNotFound
 
-# Catch the missing pkg_resources and patch it dynamically
 try:
-    import pkg_resources
-except ImportError:
-    import importlib.metadata
-
-    class _MockPkgResources:
-        class DistributionNotFound(Exception):
-            pass
-
-        @staticmethod
-        def get_distribution(package_name):
-            from collections import namedtuple
-
-            Distribution = namedtuple("Distribution", ["version"])
-            try:
-                version = importlib.metadata.version(package_name)
-                return Distribution(version=version)
-            except importlib.metadata.PackageNotFoundError:
-                raise _MockPkgResources.DistributionNotFound()
-
-    # Inject our mock into the system modules
-    sys.modules["pkg_resources"] = _MockPkgResources()
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
