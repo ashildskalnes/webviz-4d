855a6adc324614c8d7bfa61a5933c93df8551588
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2026-03-26 13:17:21 +0100

    wip

--- webviz_4d/__init__.py
+++ webviz_4d/__init__.py
@@ -1,7 +1,25 @@
-from importlib.metadata import PackageNotFoundError, version
+import sys
 
+# Catch the missing pkg_resources and patch it dynamically
 try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    pass
+    import pkg_resources
+except ImportError:
+    import importlib.metadata
+
+    class _MockPkgResources:
+        class DistributionNotFound(Exception):
+            pass
+
+        @staticmethod
+        def get_distribution(package_name):
+            from collections import namedtuple
+
+            Distribution = namedtuple("Distribution", ["version"])
+            try:
+                version = importlib.metadata.version(package_name)
+                return Distribution(version=version)
+            except importlib.metadata.PackageNotFoundError:
+                raise _MockPkgResources.DistributionNotFound()
+
+    # Inject our mock into the system modules
+    sys.modules["pkg_resources"] = _MockPkgResources()
